05df66a3dde1e85d0283631202a3f0f492b486ad
hiren1604 <waghelahien16@gmail.com>
2024-08-27 10:42:47 +0530

    Added Features

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
+  content: ['./index.html'],  // Corrected to 'index.html'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#3238f2'
+      },
+      fontFamily: {
+        'display': ['Poppins', 'sans-serif'],  
+        'body': ['Inter', 'sans-serif']
+      },
+      boxShadow: {
+        'custom': '0px 2px 10px rgba(0,0,0,0.1)'
+      }
+    },
   },
   plugins: [],
 }
