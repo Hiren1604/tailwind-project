58c7706a84d3399cb63056d934ea34db7bfd577e
hiren1604 <waghelahien16@gmail.com>
2024-09-05 20:41:35 +0530

    Added Features

--- script.js
+++ script.js
@@ -67,6 +67,19 @@ dtEls.forEach(element => {
     });
 });
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        const offsetPosition = target.offsetTop - document.querySelector('nav').offsetHeight;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
 
 
 
